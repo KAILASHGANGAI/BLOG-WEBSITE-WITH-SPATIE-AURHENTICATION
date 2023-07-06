3f866e5731779e6ebdd1ea89fd3c76d5d1d8890d
KAILASHGANGAI <47468706+KAILASHGANGAI@users.noreply.github.com>
2023-07-06 17:40:35 +0545

    featured testing

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,16 +2,24 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use App\Models\blogs;
+use App\Models\User;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
+     use RefreshDatabase;
     public function test_the_application_returns_a_successful_response(): void
     {
+       $user= User::create([
+           'name'=>'kailash',
+           'email'=>'kailash@gmail.com',
+           'phone'=>'98745698',
+           'password'=>'password'
+        ]);
+        $user->assignRoles('admin');
         $response = $this->get('/');
 
         $response->assertStatus(200);
